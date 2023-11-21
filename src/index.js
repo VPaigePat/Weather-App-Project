ef78dc05e7a8ad9340dc9440b243a8e15797b0ce
Virginia <150845139+VPaigePat@users.noreply.github.com>
2023-11-20 19:31:03 -0500

    updated HTML and Javascript
    Removed the future forecast for the moment and added the the current date. Injected more Weather Data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+function formateDate(date){
+    let hours = date.getHourse();
+    let minutes = date.getMinutes();
+    let day = days[date.getDay()];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    return `${day} ${hours} ${minutes}`;
+}
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
@@ -16,10 +25,16 @@ function updateWeatherInfo(response) {
     let refreshTemperature = document.querySelector("#city-temperature");
     let refreshHumidity = document.querySelector("#city-humidity");
     let refreshWind = document.querySelector("#city-wind");
+    let updateDescription = document.querySelector("#description");
+    let updateTime = document.querySelector("#time");
+    let date = new Date (response.data.time * 1000);
     refreshTemperature.innerHTML = response.data.temperature.current;
     refreshHumidity.innerHTML = `${response.data.temperature.humidity}%`;
     refreshWind.innerHTML = `${response.data.wind.speed} km/h`;
+    updateDescription.innerHTML = response.data.condition.description;
+    updateTime.innerHTML = formateDate(date);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
